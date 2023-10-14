36ade39634cd7f6c05b8d4a4511d15a7e9ef2f5a
Elduha07 <el0708duha@gmail.com>
2023-10-14 23:21:57 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae blanditiis, tenetur ab eos quibusdam pariatur tempore reprehenderit necessitatibus. Est amet accusamus doloribus, laborum modi delectus quidem eum error minus ab!</p>
 </body>
 </html>
