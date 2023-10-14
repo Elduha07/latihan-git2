a1e52655f7ac494584458f7a6164f344a844490f
Elduha07 <el0708duha@gmail.com>
2023-10-14 23:40:19 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae blanditiis, tenetur ab eos quibusdam pariatur tempore reprehenderit necessitatibus. Est amet accusamus doloribus, laborum modi delectus quidem eum error minus ab!</p>
+    <ul>
+        <li>Satu</li>
+        <li>Dua</li>
+    </ul>
 </body>
 </html>
